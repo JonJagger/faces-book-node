e04fa0bbae52ad1ae1d285880e3f1d74626ba37e
Jon Jagger <jon@jaggersoft.com>
2018-08-13 04:58:57 +0100

    parameterize team-city cd-pipe with port number

--- sh/team_city_cd_pipe.sh
+++ sh/team_city_cd_pipe.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 set -e
 
-# This will need to be SCP'd onto the deployment
-# machine before being SSH executed...
-# Or could it be piped into sh?
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+source ${MY_DIR}/../.env
 
-# There are many groups deploying to the same VM...
-# They each need their own
-# o) folder on the VM to store this script.
-#    Can't ssh < script as there seems to be no option to do that
-# o) their own image name. This means it must have been pushed as a unique name
-# o) their own specific port.
+docker pull ${DOCKER_REGISTRY_URL}/${FACES_BOOK_IMAGE}
 
-readonly PORT=$1
-
-docker pull dockerreg.training.local:5000/faces-book
-
-docker rm --force faces-book &> /dev/null || true
+docker rm --force ${FACES_BOOK_CONTAINER} &> /dev/null || true
 
 docker run \
   --detach \
-  --name faces-book \
-  --publish ${PORT}:${PORT} \
-  --env FACES_BOOK_PORT=${PORT} \
-    dockerreg.training.local:5000/faces-book \
+  --name ${FACES_BOOK_CONTAINER} \
+  --publish ${FACES_BOOK_PORT}:${FACES_BOOK_PORT} \
+  --env FACES_BOOK_PORT=${FACES_BOOK_PORT} \
+    ${DOCKER_REGISTRY_URL}/${FACES_BOOK_IMAGE} \
       sh -c "./faces-book.sh"
 
 sleep 2
